91704b35a1c42da11b8af842dbed1ab79ff3d765
Niciward <niciward@gmail.com>
2013-03-06 12:42:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Michaelwardfashions::Application.config.secret_token = 'fa5158f5e53f16f7c3ca3bfc37b70ec3e0b293f8bc60c6f0e1205175db40d75d404df8d6ba340250df9e841229d4bca804a9f0da3cb8fcff331bda4a6226ca55'
